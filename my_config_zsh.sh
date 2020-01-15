8d6a3e10d265c73f97f25a482984722cd0011e1c
Julian Pedro F. Braga <julian.pedro@hotmail.com>
2020-01-14 22:46:23 -0400

    Hotfix

--- my_config_zsh.sh
+++ my_config_zsh.sh
@@ -5,7 +5,7 @@ sudo apt install zsh
 echo "Installed!"
 
 echo "Installing Oh My ZSH!"
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh; zsh
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 echo "Installed!"
 
 echo "By setting ZSH as the default shell!"
@@ -13,17 +13,15 @@ sudo usermod --shell $(which zsh) $USER
 echo "OK!"
 
 echo "Installing ZSH Syntax Highlighting!"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "Installed!"
 
 echo "Installing ZSF Autosuggestions!"
-git clone https://github.com/zsh-users/zsh-autosuggestions
-$ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 echo "Installed!"
 
 echo "Installing Fzf!"
-sudp apt install fzf
+sudo apt install fzf
 echo "Installed!"
 
 echo "Installing Hack Fonts!"
